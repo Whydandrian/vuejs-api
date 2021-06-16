50562b53a94aece0efef4e2071c15d13d8b0a210
whydandrian <whydandrian@gmail.com>
2021-06-16 20:16:39 +0700

    show data & create data finished with vue3

---
+++ client-akunt/src/views/transaction/Create.vue
@@ -0,0 +1,98 @@
+<template>
+        <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <router-link :to="{ name: 'transaction.index' }"
+                class="btn btn-warning btn-sm rounded shadow mb-3">
+                    Back
+                </router-link>
+
+                <div class="card rounded shadow">
+                    <div class="card-header">
+                        Create Transaction
+                    </div>
+
+                    <div class="card-body">
+                        <form @submit.prevent="store()">
+                            <div class="mb-3">
+                                <label for="" class="form-label">Title</label>
+                                <input type="text" class="form-control" v-model="transaction.title">
+                                <div class="text-danger">
+                                    Validation Message
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Amount</label>
+                                <input type="number" class="form-control" v-model="transaction.amount">
+                                <div class="text-danger">
+                                    Validation Message
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Time</label>
+                                <input type="text" class="form-control" placeholder="yyyy-mm-dd hh:mm:ss" v-model="transaction.time">
+                                <div class="text-danger">
+                                    Validation Message
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="" class="form-label">Type</label>
+                                <select id="" class="form-control" v-model="transaction.type">
+                                    <option value="expense">Expense</option>
+                                    <option value="revenue">Revenue</option>
+                                </select>
+                                <div class="text-danger">
+                                    Validation Message
+                                </div>
+                            </div>
+                            <button class="btn btn-outline-success">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+export default {
+
+    setup(){
+        
+        const transaction = reactive({
+            title: '',
+            amount: '',
+            time: '',
+            type: '',
+        });
+
+        const validation = ref([]);
+
+        const router = useRouter();
+
+        function store() {
+            axios.post(
+                'http://localhost:8000/api/transaction',
+                transaction
+            )
+            .then(() => {
+                router.push({
+                    name: 'transaction.index'
+                });
+            }).catch((err) => {
+                validation.value = err.response.data
+            });
+        }
+
+        return {
+            transaction,
+            validation,
+            router,
+            store
+        }
+    }
+}
+</script>
